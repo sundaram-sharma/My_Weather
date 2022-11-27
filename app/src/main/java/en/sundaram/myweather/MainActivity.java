d966d809d9b34323b0be42240ccedd46255982ab
Sundaram Sharma <sundaram-sharma@hotmail.com>
2022-11-28 03:00:39 +0530

    #Functionality updated .MainActivity.java .WeatherRVAdapter.java
    #Permission Added
    .Internet
    .network
    .location coarse
    .location fine
    
    #UI updated
    .weather_rv_item.xml

--- app/src/main/java/en/sundaram/myweather/MainActivity.java
+++ app/src/main/java/en/sundaram/myweather/MainActivity.java
@@ -3,14 +3,22 @@ package en.sundaram.myweather;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+
+import en.sundaram.myweather.adapter.WeatherRVAdapter;
+import en.sundaram.myweather.model.WeatherRVModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +29,18 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView weatherRV;
     private TextInputEditText cityEdt;
     private ImageView backIV, iconIV, searchIV;
+    private ArrayList<WeatherRVModel> weatherRVModelArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //to the full view of the application
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
         homeRL = findViewById(R.id.idRLHome);
         loadingPB = findViewById(R.id.idPBLoading);
@@ -37,8 +52,15 @@ public class MainActivity extends AppCompatActivity {
         backIV = findViewById(R.id.idIVBack);
         iconIV = findViewById(R.id.idIVIcon);
         searchIV = findViewById(R.id.idIVSearch);
+        weatherRVModelArrayList = new ArrayList<>();
+        weatherRVAdapter = new WeatherRVAdapter(this, weatherRVModelArrayList);
+        weatherRV.setAdapter(weatherRVAdapter);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+    }
 
+    private void getWeatherInfo(String cityName){//inserting string name in the url given
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=3e8fa1c0c11943d7924174845222711&q=" +cityName+ "&days=1&aqi=yes&alerts=yes\n";
     }
 }
